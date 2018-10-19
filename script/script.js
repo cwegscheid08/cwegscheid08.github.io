618c441bf2b9e1fb7bb5f981544303b56593d86d
Chris Wegscheid <cwegscheid08@outlook.com>
2018-10-19 16:01:37 -0400

    Initial HTML setup, Timer Display Initial, Files Linked

--- script/script.js
+++ script/script.js
@@ -1,1 +1,37 @@
-alert("Hello World!");
+var container = document.querySelector("container");
+var display = document.querySelector("h1");
+var hr = 0;
+var min = 0;
+var sec = 0;
+
+function displayTimer() {
+	if(hr === 0 || hr === "00") {
+		display.innerHTML = min + ":" + sec;
+	} else {
+		display.innerHTML = hr + ":" + min + ":" + sec;
+	}
+}
+
+function setWorkTime(workTimeInput) {
+	convertSeconds(workTimeInput);
+	displayTimer();
+}
+
+function setRestTime(restTimeInput) {
+	convertSeconds(restTimeInput);
+	displayTimer();
+}
+
+function convertSeconds(seconds) {
+	hr = Math.floor(seconds/3600);
+	min = Math.floor((seconds - (hr * 3600))/60);
+	sec = seconds - ((hr * 3600) + (min*60));
+
+	hr >= 10 ? hr : hr = "0" + hr;
+	min >= 10 ? min : min = "0" + min;
+	sec >= 10 ? sec : sec = "0" + sec;
+}
+
+function timer() {
+	alert(Date.new);
+}
